9a416103e88d3824b71a49b00ec491a7c81d52da
Amy Pond <arias@spu.edu>
2025-06-04 17:03:21 +0000

    done with person and dynamic_cast

--- main.cpp
+++ main.cpp
@@ -7,19 +7,36 @@
 using std::cout;
 using std::endl;
 int main() {
-	Person* a = new Person("Juan", "ABC12", 20);
-	Person* b = new Student("Jean", 20, "Industrial");
-	Person* c = new Professor("Jenny", 33, "Psycho", 20000);
+	Person* p[3];
+	p[0] = new Person("Juan", "ABC12", 20);
+	p[1] = new Student("Jean", 20, "Industrial");
+	p[2] = new Professor("Jenny", 33, "Psycho", 20000);
 
-	cout << a->GetID() << endl;
-	cout << b->GetID() << endl;
-	cout << c->GetID() << endl;
+	for (int i=0; i<3; i++)
+		cout << p[i]->GetID() << endl;
+
+	//cout << "EL GPA DE Jean es: " << b->GetGPA() << endl;
+	
+	for (int i=0; i<3; i++){
+		Student* s = dynamic_cast<Student*>(p[i]);
+		if (s != nullptr){
+			cout << s->GetGPA() << endl;
+		}else{
+			Professor* r = dynamic_cast<Professor*>(p[i]);
+			if (r != nullptr){
+				cout << r->Raise(0) << endl;
+			}else{
+				cout << "Solo es person" << endl;
+			}
+		}
+		
+	}
 
 
-	delete a;
-	delete b;
-	delete c;
 
+	
+	for (int i=0; i<3; i++)
+		delete p[i];
 
 	return 0;
 }
