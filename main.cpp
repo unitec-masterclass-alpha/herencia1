c0c38f66e57e021c857ba09cc95d084618c612d5
Amy Pond <arias@spu.edu>
2025-06-04 16:52:49 +0000

    tested GetID from person

--- main.cpp
+++ main.cpp
@@ -7,30 +7,19 @@
 using std::cout;
 using std::endl;
 int main() {
-	Person p("Pamela", 19);
-	Student c("Cesar", 20, "Publicidad", 99);
-	Professor d("Carlos", 35, "Ing. Sistemas", 30000);
+	Person* a = new Person("Juan", "ABC12", 20);
+	Person* b = new Student("Jean", 20, "Industrial");
+	Person* c = new Professor("Jenny", 33, "Psycho", 20000);
 
-	cout << p.ToString() << endl;
-	cout << c.ToString() << endl;
-	cout << d.ToString() << endl;
+	cout << a->GetID() << endl;
+	cout << b->GetID() << endl;
+	cout << c->GetID() << endl;
 
-	cout << string(80, '-') << endl;
-
-	// Pasar person como parametro por valor y por referencia a una funcion
-	// Usar STL vector o list o map ?
-	// Git... branches y pull requests
-	Person* a = new Person("Lisbeth", 18);
-	Person* b = new Student("Ariel", 18, "Turismo", 85);
-	Person* e = new Professor("Mini Me", 40, "Computer Science", 50000);
-
-	cout << a->ToString() << endl; // Static Binding
-	cout << b->ToString() << endl;
-	cout << e->ToString() << endl;
 
 	delete a;
 	delete b;
-	delete e;
+	delete c;
+
 
 	return 0;
 }
